a04b62fbb09a2e5637cb59f6a55b3dca59663bb3
Evgenii Vtorushin <evgeniivtorushin@yandex.ru>
2025-10-17 13:49:56 +0300

    App

--- src/App.java
+++ src/App.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Bye");
+        System.out.println("Выберите режим: ");
+        System.out.println("1 - шифрование");
+        System.out.println("2 - дешифрование");
+        System.out.println("3 - brute force");
+        Scanner sc = new Scanner(System.in);
+        int mode = sc.nextInt();
+        sc.nextLine();
     }
 }
